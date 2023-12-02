2bcd1c57b4fd3109741402f897b51caf644a9704
Shaswath Patil <shasinfy@gmail.com>
2023-12-02 12:01:35 +0530

    Abstracting Vertex and Index Buffers

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,26 +5,11 @@
 #include <string>
 #include <sstream>
 
-#define ASSERT(x) if(!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-    x;\
-    ASSERT(GLLogCall(#x,__FILE__,__LINE__))
+#include"Renderer.h"
+#include "IndexBuffer.h"
+#include"VertexBuffer.h"
 
 
-static void GLClearError() {
-
-    while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function,const char* file,int line) {
-
-    while (GLenum error = glGetError())
-    {
-        std::cout << "[OpenGl Error] (" << error << ") :"<<function<<" "<<file<<":"<< line<<  "\n";
-        return false;
-    }
-    return true;
-}
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
@@ -148,18 +133,12 @@ int main(void)
     GLCall(glGenVertexArrays(1, &vao));
     GLCall(glBindVertexArray(vao));
 
-    unsigned int buffer;
-    GLCall(glGenBuffers(1, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float),positions,GL_STATIC_DRAW));
+    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
     glEnableVertexAttribArray(0);
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
-    unsigned int ibo;
-    GLCall(glGenBuffers(1, &ibo));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+    IndexBuffer ib(indices, 6);
 
     
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
@@ -189,7 +168,9 @@ int main(void)
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
         GLCall(glBindVertexArray(vao));
-        
+
+        ib.Bind();
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
         
         if (r > 1.0f)
