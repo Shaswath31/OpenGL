0dad1199b7d6029eb03a8be9b8cf5154407cadc6
Shaswath Patil <shasinfy@gmail.com>
2023-12-05 12:01:08 +0530

    Orthographic projection

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -13,6 +13,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include"glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 
 int main(void)
 {
@@ -28,7 +31,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -72,9 +75,11 @@ int main(void)
 
     IndexBuffer ib(indices, 6);
 
+    glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.0f, 1.0f);
     Shader shader("res/shaders/Basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    shader.SetUniformMat4f("u_MVP", proj);
 
     Texture texture("res/textures/raccoon.png");
     texture.Bind();
